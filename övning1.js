ce41632ef4f63163db50075f29682fb4f32bb401
tartagliajk <jeonniekimvazae@gmail.com>
2022-09-27 10:43:02 +0200

    Update övning1.js

--- övning1.js
+++ övning1.js
@@ -1,1 +1,7 @@
 //Skapa en funktion som tar in en array och skriver ut längden på den:
+let array = [1,2,3,4,5,6,7,8,9];
+function length(array){
+    let arr = array.length
+    return arr
+}
+console.log(length(array));
