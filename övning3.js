a81178e5429dd4d004dda803d82afec9bbd55a6d
tartagliajk <jeonniekimvazae@gmail.com>
2022-10-11 10:24:32 +0200

    -
    -

--- övning3.js
+++ övning3.js
@@ -3,9 +3,9 @@ Skapa en funktion som tar in array och skriver ut det minsta och största talet*
 let array = [1,5,2,3,6,8,7];
 function number(){
     let sortedArray = array.sort()
-    let längd = array.length
+    let length = array.length
     console.log(sortedArray)
     console.log(array[0])
-    console.log(array[längd-1])
+    console.log(array[lenght-1])
 }
 number(array)
--- övning4.js
+++ övning4.js
@@ -1,1 +1,11 @@
 /*Skapa en funktion som tar in två arrayer och skriver true om de är identiska*/
+let x = compare([1,2,3,4], [1,2,3,4]);
+function compare(a,b){
+    if (a == b){
+        console.log("TRUE")
+    }if (a != b){
+        console.log("FALSE")
+    }
+};
+
+compare(x)
